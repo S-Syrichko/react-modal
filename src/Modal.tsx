e4768770c38f20fbe69f428b5a5dd3649a9b3d8c
S-Syrichko <s-syrichko@protonmail.com>
2023-06-26 12:45:03 +0200

    feat: Added a merger for CSS properties to allow more versatile styling

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -24,45 +24,64 @@ const Modal = ({
   }
 
   const defaultCloseButton = <span>X</span>;
-  const backdropInlineStyle: React.CSSProperties = backdropStyle
-    ? backdropStyle
-    : {
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        backgroundColor: "rgba(0, 0, 0, 0.5)",
-        zIndex: 9998,
-      };
-  const modalInlineStyle: React.CSSProperties = modalStyle
-    ? modalStyle
-    : {
-        position: "relative",
-        color: "#000",
-        backgroundColor: "#fff",
-        padding: "20px",
-        borderRadius: "8px",
-        zIndex: 9999,
-      };
-  const closeInlineStyle: React.CSSProperties = closeStyle
-    ? closeStyle
-    : {
-        position: "absolute",
-        top: "10px",
-        right: "10px",
-        cursor: "pointer",
-      };
+  const defaultStyles: {
+    backdrop: React.CSSProperties;
+    modal: React.CSSProperties;
+    close: React.CSSProperties;
+  } = {
+    backdrop: {
+      position: "fixed",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      backgroundColor: "rgba(0, 0, 0, 0.5)",
+      zIndex: 9998,
+    },
+    modal: {
+      position: "relative",
+      color: "#000",
+      backgroundColor: "#fff",
+      padding: "20px",
+      borderRadius: "8px",
+      zIndex: 9999,
+    },
+    close: {
+      position: "absolute",
+      top: "10px",
+      right: "10px",
+      cursor: "pointer",
+    },
+  };
+
+  const mergedBackdropStyle: React.CSSProperties = {
+    ...defaultStyles.backdrop,
+    ...backdropStyle,
+  };
+
+  const mergedModalStyle: React.CSSProperties = {
+    ...defaultStyles.modal,
+    ...modalStyle,
+  };
+
+  const mergedCloseStyle: React.CSSProperties = {
+    ...defaultStyles.close,
+    ...closeStyle,
+  };
 
   return (
     <div className="modal-overlay">
       <div
         className="modal-backdrop"
-        style={backdropInlineStyle}
+        style={mergedBackdropStyle}
         onClick={onClose}
       ></div>
-      <div className="modal" style={modalInlineStyle}>
-        <button className="modal-close" style={closeInlineStyle} onClick={onClose}>
+      <div className="modal" style={mergedModalStyle}>
+        <button
+          className="modal-close"
+          style={mergedCloseStyle}
+          onClick={onClose}
+        >
           {closeButton ? closeButton : defaultCloseButton}
         </button>
         <div className="modal-content">{children}</div>
